74a642fee705a72d73d6ca24b0047968bbb85d57
UmerIftikhar111 <chumeriftikhar11@gmail.com>
2022-11-14 13:26:00 +0500

    Deploy -> Git Pages

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import List from './List';
+import App from './App';
 
-test('renders learn react link', () => {
+test('have content test', () => {
   render(<List />);
   const linkElement = screen.getByTestId("lister");
   expect(linkElement).toHaveTextContent('Bat');
+});
+
+test('component exists test', () => {
+  render(<App />);
+  const linkElement = screen.getByTestId("comp");
+  expect(linkElement).toBeInTheDocument();
 });
--- src/List.js
+++ src/List.js
@@ -4,8 +4,10 @@ import './App.css';
 function List() {
   return (
     <div data-testid = "lister" className="list">
+     <ul>
       <h1>Bat</h1>
       <h1>Ball</h1>
+      </ul>
     </div>
      
   );
